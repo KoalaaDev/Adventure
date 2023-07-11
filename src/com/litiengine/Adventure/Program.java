45b547f699853d26fdf0f69fbbd9ea0eaf9ab2eb
KoalaaDev <Williamhow219@hotmail.com>
2023-07-12 00:04:11 +0400

    added game logo

--- src/com/litiengine/Adventure/Program.java
+++ src/com/litiengine/Adventure/Program.java
@@ -12,9 +12,13 @@ public class Program {
     Game.info().setVersion("v0.0.1");
     Game.info().setWebsite("https://github.com/KoalaaDev/Adventure");
     Game.info().setDescription("A 2D RPG Adventure Game created by Group 1 for the OOP course");
+    
+    
     Game.audio().playMusic(Resources.sounds().get("audio/looping-overcome-atmanan.mp3")); // background music
 
     Game.init(args);
+    //set the icon
+    Game.window().setIcon(Resources.images().get("images/windowlogo.png"));
     Game.start();
   }
 }
