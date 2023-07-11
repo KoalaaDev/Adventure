953aeb152002a023041fdceeeec9980e175c831e
KoalaaDev <Williamhow219@hotmail.com>
2023-07-11 21:39:59 +0400

    added background music

--- src/com/litiengine/Adventure/Program.java
+++ src/com/litiengine/Adventure/Program.java
@@ -1,10 +1,13 @@
 package com.litiengine.Adventure;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
 
 public class Program {
 
   public static void main(String[] args) {
+    Game.audio().playMusic(Resources.sounds().get("audio/looping-overcome-atmanan.mp3")); // background music
+    
     Game.init(args);
     Game.start();
   }
