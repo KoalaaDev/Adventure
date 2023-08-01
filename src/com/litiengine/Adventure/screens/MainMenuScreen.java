b547f720003a9d051f82d2e8b5fbb750e60ff316
Rasesh Rampersand <rasesh.rampersand@umail.uom.ac.mu>
2023-08-01 23:30:39 +0400

    Main Menu Screen

--- src/com/litiengine/Adventure/screens/MainMenuScreen.java
+++ src/com/litiengine/Adventure/screens/MainMenuScreen.java
@@ -1,4 +1,61 @@
 package com.litiengine.Adventure.screens;
 
-public class MainMenuScreen {
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
+import de.gurkenlabs.litiengine.gui.ImageComponent;
+import de.gurkenlabs.litiengine.gui.Menu;
+import de.gurkenlabs.litiengine.gui.screens.Screen;
+import de.gurkenlabs.litiengine.resources.Resources;
+import de.gurkenlabs.litiengine.resources.SpritesheetResource;
+import de.gurkenlabs.litiengine.util.Imaging;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class MainMenuScreen extends Screen{
+
+    private ImageComponent bkgr;
+    private Menu menu;
+
+    public  MainMenuScreen(){
+        super("menu");
+
+    }
+    @Override
+    public void prepare(){
+        super.prepare();
+        Game.audio().playMusic(Resources.sounds().get("audio/looping-overcome-atmanan.mp3"));
+    }
+
+    @Override
+    protected void initializeComponents(){
+        super.initializeComponents();
+        final BufferedImage buttonImg = Imaging.scale(Resources.images().get("images/menu_item.png"), .5f);
+        final Spritesheet button = new Spritesheet(buttonImg, "menu_item.png", buttonImg.getWidth(), buttonImg.getHeight());
+        final String[] items = { "Neww Game", "Quit" };
+        bkgr = new ImageComponent(0, 0, Resources.images().get("image/menu.png"));
+        menu = new Menu((Game.window().getWidth() - buttonImg.getWidth()) / 2d,
+                (Game.window().getHeight() - buttonImg.getHeight() * items.length) / 2d, buttonImg.getWidth(), buttonImg.getHeight() * items.length,
+                button, items);
+
+        menu.onChange(index -> {
+            if(index == 0){
+                Game.world().loadEnvironment("map1");
+                Game.start();
+            }
+            if(index == 1){
+                System.exit(0);
+            }
+        });
+        for (ImageComponent cell : menu.getCellComponents()) {
+
+
+        }
+        getComponents().add(bkgr);
+        getComponents().add(menu);
+
+
+
+    }
+
 }
