877f13f9eaef4bfbecf09ba046d8927a1a7253ec
Rasesh Rampersand <rasesh.rampersand@umail.uom.ac.mu>
2023-08-13 21:48:13 +0400

    add new comment

--- src/com/litiengine/Adventure/screens/TitleScreen.java
+++ src/com/litiengine/Adventure/screens/TitleScreen.java
@@ -1,6 +1,5 @@
 package com.litiengine.Adventure.screens;
 
-
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
@@ -9,34 +8,45 @@ import de.gurkenlabs.litiengine.resources.Resources;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-
-
+/**
+ * The TitleScreen class represents the title screen of the game.
+ * This screen typically appears when the game is launched and provides options for starting the game or accessing other features.
+ */
 public class TitleScreen extends Screen {
 
-private ImageComponent startGame;
+    // The component representing the "Start Game" button
+    private ImageComponent startGame;
 
-public TitleScreen(){
-    super("title");
-}
+    // Constructor
+    public TitleScreen() {
+        super("title"); // Set a unique identifier for this screen
+    }
 
-@Override
-    public void render(Graphics2D g){
+    @Override
+    public void render(Graphics2D g) {
+        super.render(g); // Render the base screen
+        startGame.render(g); // Render the "Start Game" button component
+    }
 
-    super.render(g);
-    //g.drawImage(Resources.images().get("images/map.png"),0,0,Game.window().getWidth(), Game.window().getHeight(),null);
-    startGame.render(g);
-}
+    @Override
+    protected void initializeComponents() {
+        super.initializeComponents(); // Initialize components of the base screen
 
-    @Override protected void initializeComponents() {
-        super.initializeComponents();
+        // Load the button image and create the "Start Game" button component
         BufferedImage button = Resources.images().get("images/Screenshot 2023-08-07 at 22.05.28.png");
-        startGame = new ImageComponent((Game.window().getWidth() + 100) , (Game.window().getHeight() - 100 ) , button);
+        startGame = new ImageComponent(Game.window().getWidth() + 100, Game.window().getHeight() - 100, button);
         startGame.setText("Start Game");
+
+        // Add a click event handler to transition to the "menu" screen when the button is clicked
         startGame.onClicked(event -> {
             suspend();
             Game.screens().display("menu");
         });
+
+        // Set a hover sound for the button
         startGame.setHoverSound(Resources.sounds().get("audio/click.mp3"));
+
+        // Add the "Start Game" button component to the screen's components
         getComponents().add(startGame);
     }
 }
