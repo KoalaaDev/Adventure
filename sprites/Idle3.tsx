82fea29c81a4690ab498e2e840ee7d6b294001ee
Sheetul27 <123946314+Sheetul27@users.noreply.github.com>
2023-08-14 00:28:45 +0400

    Add files via upload
    evil wizard 2

---
+++ sprites/Idle3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Idle" tilewidth="32" tileheight="32" tilecount="148" columns="37">
+ <image source="../Downloads/Evil Wizard/Sprites/Idle.png" width="1200" height="150"/>
+</tileset>
