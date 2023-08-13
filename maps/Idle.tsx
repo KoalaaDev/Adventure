4de675ea1acff7e6b64c8c13f3f0c5c673434bfb
Sheetul27 <123946314+Sheetul27@users.noreply.github.com>
2023-08-14 00:22:37 +0400

    Add files via upload
    hero warrior

---
+++ maps/Idle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Idle" tilewidth="32" tileheight="32" tilecount="434" columns="62">
+ <image source="../Downloads/EVil Wizard 2/Sprites/Idle.png" width="2000" height="250"/>
+</tileset>
