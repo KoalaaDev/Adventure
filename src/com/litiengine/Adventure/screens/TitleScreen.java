67c34afd3f671e607c100c498bccce272e515c87
Rasesh Rampersand <rasesh.rampersand@umail.uom.ac.mu>
2023-08-17 00:20:07 +0400

    change title screen

--- src/com/litiengine/Adventure/screens/TitleScreen.java
+++ src/com/litiengine/Adventure/screens/TitleScreen.java
@@ -1,50 +1,62 @@
 package com.litiengine.Adventure.screens;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.graphics.Spritesheet;
 import de.gurkenlabs.litiengine.gui.ImageComponent;
 import de.gurkenlabs.litiengine.gui.Menu;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
 import de.gurkenlabs.litiengine.resources.Resources;
+import de.gurkenlabs.litiengine.util.Imaging;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
+/**
+ * The TitleScreen class represents the title screen of the game.
+ * This screen typically appears when the game is launched and provides options for starting the game or accessing other features.
+ */
 public class TitleScreen extends Screen {
 
     // The component representing the "Start Game" button
-    private Menu titleMenu;
-
+    private ImageComponent bkgr;
+    private Menu TitleMenu;
     // Constructor
     public TitleScreen() {
         super("title"); // Set a unique identifier for this screen
     }
 
     @Override
-    protected void initializeComponents() {
-        super.initializeComponents();
+    public void prepare(){
+        super.prepare();
 
-        Game.window().getRenderComponent().addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Display the main menu screen when the screen is clicked
+    }
+
+    @Override
+    protected void initializeComponents() {
+        super.initializeComponents(); // Initialize components of the base screen
+        final BufferedImage buttonImg = Imaging.scale(Resources.images().get("images/menu_item.png"), .3f);
+        final Spritesheet button = new Spritesheet(buttonImg, "images/menu_item.png", buttonImg.getWidth(), buttonImg.getHeight());
+        final String[] items = {"Start Game"};
+        // Load the button image and create the "Start Game" button component
+        bkgr = new ImageComponent(-50, 50, Resources.images().get("images/trial2.png"));
+
+        TitleMenu = new Menu((Game.window().getWidth()  - buttonImg.getWidth()) / 2d,
+                (Game.window().getHeight() + 1350  - buttonImg.getHeight() * items.length) / 2d, buttonImg.getWidth(), buttonImg.getHeight() * items.length,
+                button, items);
+        // Add a click event handler to transition to the "menu" screen when the button is clicked
+        TitleMenu.onChange(index -> {
+            if (index == 0) {
+                suspend();
+                Game.screens().add(new MainMenuScreen());
                 Game.screens().display("menu");
             }
         });
+        // Set a hover sound for the button
+        TitleMenu.setHoverSound(Resources.sounds().get("audio/click.mp3"));
 
-        // Load and initialize the background image
-        ImageComponent backgroundImage = new ImageComponent(0, 0, Resources.images().get("images/Screenshot 2023-08-07 at 22.05.28.png"));
-        backgroundImage.setWidth(Game.window().getWidth()-50 );
-        backgroundImage.setHeight(Game.window().getHeight()+600);
-        getComponents().add(backgroundImage);
+        // Add the "Start Game" button component to the screen's components
+        getComponents().add(bkgr);
+        getComponents().add(TitleMenu);
 
-        // Initialize the title menu here if needed
-        // titleMenu = ...
     }
-
-
 }
-
-
